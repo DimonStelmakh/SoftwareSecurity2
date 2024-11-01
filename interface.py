195689217957f76cae5277196d5d8f53cf831538
Dimon Stelmakh <105310426+DimonStelmakh@users.noreply.github.com>
2024-11-01 22:33:03 +0200

    Lab 4 completed entirely

--- interface.py
+++ interface.py
@@ -1,8 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog, messagebox, ttk, TclError
-from xml.sax import parse
 
-from system import CaesarCipher, TrithemiusCipher, BookCipher
+from system import EncryptionDES, DES
 
 
 class CryptoApp(tk.Tk):
@@ -11,83 +10,40 @@ class CryptoApp(tk.Tk):
         self.title("Криптографічна система")
         self.geometry("700x500")
 
-        self.cipher_type = tk.StringVar(value='caesar')
-        self.language_var = tk.StringVar(value='ua')
-        self.key_var = tk.IntVar()
-        self.A_var = tk.IntVar()
-        self.B_var = tk.IntVar()
-        self.C_var = tk.IntVar()
-        self.key_phrase_var = tk.StringVar()
-        self.pangram_var = tk.StringVar()
+        self.mode_var = tk.StringVar(value='ECB')
+        self.key_var = tk.StringVar(value='')
+        self.iv_var = tk.StringVar(value='')
 
         self.init_ui()
         self.create_menu()
 
+
     def init_ui(self):
         self.input_text = tk.Text(self, height=5, width=60)
         self.input_text.pack(pady=10)
-
         self.output_text = tk.Text(self, height=5, width=60)
         self.output_text.pack(pady=10)
 
-        # cipher choise
-        tk.Label(self, text="Виберіть тип шифрування:").pack()
-        tk.Radiobutton(self, text="Шифр Цезаря", variable=self.cipher_type, value='caesar',
-                       command=self.toggle_cipher_options).pack()
-        tk.Radiobutton(self, text="Шифр Тритеміуса", variable=self.cipher_type, value='trithemius',
-                       command=self.toggle_cipher_options).pack()
-        tk.Radiobutton(self, text="Книжковий шифр", variable=self.cipher_type, value='book',
-                       command=self.toggle_cipher_options).pack()
+        tk.Label(self, text="Оберіть режим DES:").pack()
+        mode_options = ['ECB', 'CBC', 'CFB', 'OFB']
+        self.mode_menu = tk.OptionMenu(self, self.mode_var, *mode_options,
+                                       command=lambda _: self.toggle_des_options())
+        self.mode_menu.pack()
 
-        tk.Label(self, text="Мова:").pack()
-        self.language_menu = tk.OptionMenu(self, self.language_var, 'ua', 'en')
-        self.language_menu.pack()
+        tk.Label(self, text="Введіть 8-байтовий ключ:").pack()
+        self.key_entry = tk.Entry(self, textvariable=self.key_var, width=20)
+        self.key_entry.pack(pady=5)
 
-        self.key_frame = tk.Frame(self)
-        tk.Label(self.key_frame, text="Ключ для шифру Цезаря:").pack(side=tk.LEFT)
-        self.key_entry = tk.Entry(self.key_frame, textvariable=self.key_var)
-        self.key_entry.pack(side=tk.LEFT)
-        self.key_frame.pack(pady=5)
+        self.iv_frame = tk.Frame(self)
+        tk.Label(self.iv_frame, text="Також введіть 8-байтовий IV (для режимів CBC, CFB, OFB):").pack()
+        self.iv_entry = tk.Entry(self.iv_frame, textvariable=self.iv_var, width=20)
+        self.iv_frame.pack(pady=5)
 
-        self.trithemius_frame = tk.Frame(self)
-        self.method_label = tk.Label(self.trithemius_frame, text="Метод шифру Тритеміуса:")
-        self.method_label.pack(side=tk.LEFT)
-        self.method_choice = ttk.Combobox(self.trithemius_frame,
-                                          values=['Лінійне рівняння', 'Нелінійне рівняння', 'Гасло'])
-        self.method_choice.pack(side=tk.LEFT)
-        self.method_choice.bind("<<ComboboxSelected>>", self.show_trithemius_options)
+        tk.Button(self, text="Зашифрувати", command=self.encrypt_text).pack(pady=5)
+        tk.Button(self, text="Розшифрувати", command=self.decrypt_text).pack(pady=5)
 
-        # coefficients
-        self.coefficients_frame = tk.Frame(self)
-        tk.Label(self.coefficients_frame, text="A:").pack(side=tk.LEFT)
-        self.A_entry = tk.Entry(self.coefficients_frame, textvariable=self.A_var)
-        self.A_entry.pack(side=tk.LEFT)
-        tk.Label(self.coefficients_frame, text="B:").pack(side=tk.LEFT)
-        self.B_entry = tk.Entry(self.coefficients_frame, textvariable=self.B_var)
-        self.B_entry.pack(side=tk.LEFT)
-        tk.Label(self.coefficients_frame, text="C:").pack(side=tk.LEFT)
-        self.C_entry = tk.Entry(self.coefficients_frame, textvariable=self.C_var)
-        self.C_entry.pack(side=tk.LEFT)
+        self.toggle_des_options()
 
-        # key phrase
-        self.key_phrase_frame = tk.Frame(self)
-        tk.Label(self.key_phrase_frame, text="Гасло:").pack(side=tk.LEFT)
-        self.key_phrase_entry = tk.Entry(self.key_phrase_frame, textvariable=self.key_phrase_var)
-        self.key_phrase_entry.pack(side=tk.LEFT)
-
-        # pangram
-        self.pangram_frame = tk.Frame(self)
-        tk.Label(self.pangram_frame, text="Вірш (бажано панграма):").pack(side=tk.LEFT)
-        self.pangram_entry = tk.Entry(self.pangram_frame, textvariable=self.pangram_var)
-        self.pangram_entry.pack(side=tk.LEFT)
-
-        self.pangram_info_frame = tk.Frame(self)
-        tk.Label(self.pangram_info_frame, text='Якщо не ввести вірш, буде використано стандартний (див. "Допомога")').pack(side=tk.TOP)
-
-        tk.Button(self, text="Шифрувати", command=self.encrypt_file).pack(pady=5)
-        tk.Button(self, text="Розшифрувати", command=self.decrypt_file).pack(pady=5)
-
-        self.toggle_cipher_options()
 
     def create_menu(self):
         menubar = tk.Menu(self)
@@ -103,7 +59,7 @@ class CryptoApp(tk.Tk):
         # меню "Допомога"
         help_menu = tk.Menu(menubar, tearoff=0)
         help_menu.add_command(label="Про розробника", command=self.about_developer)
-        help_menu.add_command(label="Стандартний ключ книжкового шифру", command=self.standard_book_key)
+        # help_menu.add_command(label="Стандартний ключ книжкового шифру", command=self.standard_book_key)
         menubar.add_cascade(label="Допомога", menu=help_menu)
 
         self.config(menu=menubar)
@@ -126,161 +82,54 @@ class CryptoApp(tk.Tk):
     def quit_app(self):
         self.quit()
 
-    def toggle_cipher_options(self):
-        cipher = self.cipher_type.get()
-        if cipher == 'caesar':
-            self.pangram_frame.pack_forget()
-            self.pangram_info_frame.pack_forget()
-            self.trithemius_frame.pack_forget()
-            self.coefficients_frame.pack_forget()
-            self.key_phrase_frame.pack_forget()
-            self.key_frame.pack(pady=5)
-        elif cipher == 'trithemius':
-            self.key_frame.pack_forget()
-            self.pangram_frame.pack_forget()
-            self.pangram_info_frame.pack_forget()
-            self.trithemius_frame.pack(pady=5)
-        elif cipher == 'book':
-            self.key_frame.pack_forget()
-            self.trithemius_frame.pack_forget()
-            self.coefficients_frame.pack_forget()
-            self.key_phrase_frame.pack_forget()
-            self.pangram_frame.pack(pady=5)
-            self.pangram_info_frame.pack(pady=5)
+    def toggle_des_options(self):
+        selected_mode = self.mode_var.get()
+        if selected_mode == 'ECB':
+            self.iv_frame.pack_forget()
+        else:
+            self.iv_frame.pack(pady=5)
+            for widget in self.iv_frame.winfo_children():
+                widget.pack()
 
-    def show_trithemius_options(self, event=None):
-        method = self.method_choice.get()
-        if method == 'Лінійне рівняння':
-            self.coefficients_frame.pack(pady=5)
-            self.key_phrase_frame.pack_forget()
-            self.C_entry.config(state=tk.DISABLED)
-        elif method == 'Нелінійне рівняння':
-            self.coefficients_frame.pack(pady=5)
-            self.key_phrase_frame.pack_forget()
-            self.C_entry.config(state=tk.NORMAL)
-        elif method == 'Гасло':
-            self.coefficients_frame.pack_forget()
-            self.key_phrase_frame.pack(pady=5)
+        # self.update_idletasks()
 
-    def encrypt_file(self):
-        cipher_type = self.cipher_type.get()
-        input_text = self.input_text.get("1.0", tk.END).strip()
-        language = self.language_var.get()
+    def encrypt_text(self):
+        key = self.key_var.get().encode().ljust(8, b'\0')[:8]  # забезпечуємо завжди 8 байтів
+        plaintext = self.input_text.get("1.0", tk.END).strip().encode()
 
-        if cipher_type == 'caesar':
-            try:
-                key = self.key_var.get()
-            except TclError:
-                messagebox.showerror(title="Неправильний тип!", message="Ключ повинен бути цілим числом!")
-                return
-            cipher = CaesarCipher(key)
-            encrypted_text = cipher.encrypt(input_text, language=language)
+        mode = getattr(DES, 'MODE_' + self.mode_var.get())
 
-        elif cipher_type == 'trithemius':
-            method = self.method_choice.get()
-            if method == 'Лінійне рівняння':
-                try:
-                    A, B = self.A_var.get(), self.B_var.get()
-                except TclError:
-                    messagebox.showerror(title="Неправильний тип!", message="Коефіцієнти повинні бути цілими числами!")
-                    return
-                cipher = TrithemiusCipher(A=A, B=B)
-                encrypted_text = cipher.encrypt_linear(input_text, language=language)
-            elif method == 'Нелінійне рівняння':
-                try:
-                    A, B, C = self.A_var.get(), self.B_var.get(), self.C_var.get()
-                except TclError:
-                    messagebox.showerror(title="Неправильний тип!", message="Коефіцієнти повинні бути цілими числами!")
-                    return
-                cipher = TrithemiusCipher(A=A, B=B, C=C)
-                encrypted_text = cipher.encrypt_nonlinear(input_text, language=language)
-            elif method == 'Гасло':
-                try:
-                    key_phrase = self.key_phrase_var.get()
-                except TclError:
-                    messagebox.showerror(title="Неправильний тип!", message="Гасло повинне бути стрічкою!")
-                    return
-                cipher = TrithemiusCipher(key_phrase=key_phrase)
-                encrypted_text = cipher.encrypt_key_phrase(input_text, language=language)
+        if self.mode_var.get() == 'ECB':
+            des_cipher = EncryptionDES(mode, key)
+        else:
+            iv = self.iv_var.get().encode().ljust(8, b'\0')[:8]
+            des_cipher = EncryptionDES(mode, key, iv=iv)
 
-        elif cipher_type == 'book':
-            try:
-                pangram = self.pangram_var.get()
-            except TclError:
-                messagebox.showerror(title="Неправильний тип!", message="Вірш повинен бути стрічкою!")
-                return
-            cipher = BookCipher(pangram=pangram, language=language)
-            encrypted_text = cipher.encrypt(input_text)
+        ciphertext = des_cipher.encrypt(plaintext)
 
         self.output_text.delete("1.0", tk.END)
-        self.output_text.insert(tk.END, encrypted_text)
+        self.output_text.insert("1.0", ciphertext.hex())
 
-    def decrypt_file(self):
-        cipher_type = self.cipher_type.get()
-        input_text = self.input_text.get("1.0", tk.END).strip()
-        language = self.language_var.get()
+    def decrypt_text(self):
+        key = self.key_var.get().encode().ljust(8, b'\0')[:8]  # забезпечуємо завжди 8 байтів
+        ciphertext = bytes.fromhex(self.input_text.get("1.0", tk.END).strip())
 
-        if cipher_type == 'caesar':
-            try:
-                key = self.key_var.get()
-            except TclError:
-                messagebox.showerror(title="Неправильний тип!", message="Ключ повинен бути цілим числом!")
-                return
-            cipher = CaesarCipher(key)
-            decrypted_text = cipher.decrypt(input_text, language=language)
+        mode = getattr(DES, 'MODE_' + self.mode_var.get())
 
-        elif cipher_type == 'trithemius':
-            method = self.method_choice.get()
-            match method:
-                case 'Лінійне рівняння':
-                    try:
-                        A, B = self.A_var.get(), self.B_var.get()
-                    except TclError:
-                        messagebox.showerror(title="Неправильний тип!", message="Коефіцієнти повинні бути цілими числами!")
-                        return
-                    cipher = TrithemiusCipher(A=A, B=B)
-                    decrypted_text = cipher.decrypt_linear(input_text, language=language)
-                case 'Нелінійне рівняння':
-                    try:
-                        A, B, C = self.A_var.get(), self.B_var.get(), self.C_var.get()
-                    except TclError:
-                        messagebox.showerror(title="Неправильний тип!",
-                                             message="Коефіцієнти повинні бути цілими числами!")
-                        return
-                    cipher = TrithemiusCipher(A=A, B=B, C=C)
-                    decrypted_text = cipher.decrypt_nonlinear(input_text, language=language)
-                case 'Гасло':
-                    try:
-                        key_phrase = self.key_phrase_var.get()
-                    except TclError:
-                        messagebox.showerror(title="Неправильний тип!", message="Гасло повинне бути стрічкою!")
-                        return
-                    cipher = TrithemiusCipher(key_phrase=key_phrase)
-                    decrypted_text = cipher.decrypt_key_phrase(input_text, language=language)
-                case _:
-                    messagebox.showerror("Помилка", "Неочікувана помилка розшифрування")
+        if self.mode_var.get() == 'ECB':
+            des_cipher = EncryptionDES(mode, key)
+        else:
+            iv = self.iv_var.get().encode().ljust(8, b'\0')[:8]
+            des_cipher = EncryptionDES(mode, key, iv=iv)
 
-        elif cipher_type == 'book':
-            try:
-                pangram = self.pangram_var.get()
-            except TclError:
-                messagebox.showerror(title="Неправильний тип!", message="Вірш повинен бути стрічкою!")
-                return
-            cipher = BookCipher(pangram=pangram, language=language)
-            decrypted_text = cipher.decrypt(input_text)
+        decrypted_text = des_cipher.decrypt(ciphertext)
 
         self.output_text.delete("1.0", tk.END)
-        self.output_text.insert(tk.END, decrypted_text)
+        self.output_text.insert("1.0", decrypted_text.decode(errors='ignore'))
 
     def about_developer(self):
         messagebox.showinfo("Про розробника", "Ця система розроблена Дмитром Стельмахом із групи ТВ-13")
 
-    def standard_book_key(self):
-        cipher = BookCipher(None, 'ua')
-        ua_string = '\n'.join(cipher.ua_pangrams)
-        en_string = '\n'.join(cipher.en_pangrams)
-        messagebox.showinfo(title="Стандартні ключі книжкового шифру", message=f"Українською:\n\n{ua_string}\n\n"
-                                                                               f"Англійською:\n\n{en_string}")
 
 if __name__ == "__main__":
     app = CryptoApp()
--- system.py
+++ system.py
@@ -1,176 +1,40 @@
-import random
+from Crypto.Cipher import DES
 
-class CaesarCipher:
-    def __init__(self, key):
+
+class EncryptionDES:
+    def __init__(self, mode, key, iv=None):
+        self.mode = mode
         self.key = key
-        self.alphabet_ua = 'абвгґдеєжзиіїйклмнопрстуфхцчшщьюя'
-        self.alphabet_en = 'abcdefghijklmnopqrstuvwxyz'
+        self.iv = iv
 
-
-    def encrypt(self, text, language='ua'):
-        # self.validate_key()
-        alphabet = self.alphabet_ua if language == 'ua' else self.alphabet_en
-        shifted_alphabet = alphabet[self.key % len(alphabet):] + alphabet[:self.key % len(alphabet)]
-        table = str.maketrans(alphabet, shifted_alphabet)
-        return text.translate(table)
-
-    def decrypt(self, text, language='ua'):
-        # self.validate_key()
-        alphabet = self.alphabet_ua if language == 'ua' else self.alphabet_en
-        shifted_alphabet = alphabet[-self.key % len(alphabet):] + alphabet[:-self.key % len(alphabet)]
-        table = str.maketrans(alphabet, shifted_alphabet)
-        return text.translate(table)
-
-
-class TrithemiusCipher:
-    def __init__(self, A=0, B=0, C=0, key_phrase=None):
-        self.A = A
-        self.B = B
-        self.C = C
-        self.key_phrase = key_phrase
-        self.alphabet_ua = 'абвгґдеєжзиіїйклмнопрстуфхцчшщьюя'
-        self.alphabet_en = 'abcdefghijklmnopqrstuvwxyz'
-
-    def encrypt_linear(self, text, language='ua'):
-        # self.validate_coefficients(True)
-        alphabet = self.alphabet_ua if language == 'ua' else self.alphabet_en
-        encrypted_text = []
-        for i, char in enumerate(text):
-            if char in alphabet:
-                step = self.A + self.B * i
-                new_pos = (alphabet.index(char) + step) % len(alphabet)
-                encrypted_text.append(alphabet[new_pos])
-            else:
-                encrypted_text.append(char)
-        return ''.join(encrypted_text)
-
-    def decrypt_linear(self, text, language='ua'):
-        # self.validate_coefficients(True)
-        alphabet = self.alphabet_ua if language == 'ua' else self.alphabet_en
-        decrypted_text = []
-        for i, char in enumerate(text):
-            if char in alphabet:
-                step = self.A + self.B * i
-                new_pos = (alphabet.index(char) - step) % len(alphabet)
-                decrypted_text.append(alphabet[new_pos])
-            else:
-                decrypted_text.append(char)
-        return ''.join(decrypted_text)
-
-    def encrypt_nonlinear(self, text, language='ua'):
-        # self.validate_coefficients(False)
-        alphabet = self.alphabet_ua if language == 'ua' else self.alphabet_en
-        encrypted_text = []
-        for i, char in enumerate(text):
-            if char in alphabet:
-                step = self.A**2 + self.B * i + self.C
-                new_pos = (alphabet.index(char) + step) % len(alphabet)
-                encrypted_text.append(alphabet[new_pos])
-            else:
-                encrypted_text.append(char)
-        return ''.join(encrypted_text)
-
-    def decrypt_nonlinear(self, text, language='ua'):
-        # self.validate_coefficients(False)
-        alphabet = self.alphabet_ua if language == 'ua' else self.alphabet_en
-        decrypted_text = []
-        for i, char in enumerate(text):
-            if char in alphabet:
-                step = self.A ** 2 + self.B * i + self.C
-                new_pos = (alphabet.index(char) - step) % len(alphabet)
-                decrypted_text.append(alphabet[new_pos])
-            else:
-                decrypted_text.append(char)
-        return ''.join(decrypted_text)
-
-    def encrypt_key_phrase(self, text, language='ua'):
-        alphabet = self.alphabet_ua if language == 'ua' else self.alphabet_en
-        key_phrase_repeated = (self.key_phrase * (len(text) // len(self.key_phrase) + 1))[:len(text)]
-        encrypted_text = []
-        for char, key_char in zip(text, key_phrase_repeated):
-            if char in alphabet:
-                step = alphabet.index(key_char) + 1
-                new_pos = (alphabet.index(char) + step) % len(alphabet)
-                encrypted_text.append(alphabet[new_pos])
-            else:
-                encrypted_text.append(char)
-        return ''.join(encrypted_text)
-
-    def decrypt_key_phrase(self, text, language='ua'):
-        alphabet = self.alphabet_ua if language == 'ua' else self.alphabet_en
-        key_phrase_repeated = (self.key_phrase * (len(text) // len(self.key_phrase) + 1))[:len(text)]
-        decrypted_text = []
-        for char, key_char in zip(text, key_phrase_repeated):
-            if char in alphabet:
-                step = alphabet.index(key_char) + 1
-                new_pos = (alphabet.index(char) - step) % len(alphabet)
-                decrypted_text.append(alphabet[new_pos])
-            else:
-                decrypted_text.append(char)
-        return ''.join(decrypted_text)
-
-
-class BookCipher:
-    def __init__(self, pangram=None, language='ua'):
-        self.ua_pangrams = pangram if pangram and language=='ua' else (
-            "жебракують філософи при ґанку церкви в гадячі, ще й шатро їхнє п'яне знаємо",
-            "є місць багато, ґедзю, де фрукт в’ялий їж і шуми, а хвощ не чіпай",
-            "доки ж є чаш цих п’ять, фехтуймо ґречно, юнаки щасливі, за її губи",
-            "на подушечці форми любої є й ґудзик щоб пір’я геть жовте сховати"
-        )
-
-        self.en_pangrams = pangram if pangram and language=='en' else (
-            "the quick brown fox jumps over the lazy dog",
-            "pack my box with five dozen liquor jugs",
-            "how vexingly quick daft zebras jump",
-            "waltz, bad nymph, for quick jigs vex"
-        )
-
-        self.language = language
-        self.table = self.create_table(language)
-
-    def create_table(self, language):
-        # створюємо таблицю 10x10 з панграм (нумеруємо 0-9)
-        if language == 'ua':
-            pangram_string = ''.join(self.ua_pangrams).replace(' ', '').replace(',', '').replace('’', '').replace('-','')
+    def encrypt(self, plaintext):
+        if self.mode == DES.MODE_ECB:
+            cipher = DES.new(self.key, self.mode)
         else:
-            pangram_string = ''.join(self.en_pangrams).replace(' ', '').replace(',', '').replace('’', '').replace('-','')
+            cipher = DES.new(self.key, self.mode, iv=self.iv)
 
-        while len(pangram_string) < 100:
-            pangram_string*=2
+        padded_text = self.pad(plaintext)
+        encrypted = cipher.encrypt(padded_text)
 
-        table = [pangram_string[i:i + 10] for i in range(0, 100, 10)]
-        return table
+        return (self.iv + encrypted) if self.iv else encrypted
 
-    def encrypt(self, text):
-        encrypted_text = []
-        for char in text:
-            positions = [(i, j) for i in range(10) for j in range(10) if self.table[i][j].lower() == char.lower()]
-            if positions:
-                i, j = random.choice(positions)
-                encrypted_text.append(f"{i}/{j}")
-            else:
-                encrypted_text.append(char)
-                # якщо символ не знайдено, лишаємо як є, але бажано використовувати панграми, щоб такого не траплялось
-        return ' '.join(encrypted_text)
+    def decrypt(self, ciphertext):
+        if self.mode == DES.MODE_ECB:
+            cipher = DES.new(self.key, self.mode)
+            decrypted = cipher.decrypt(ciphertext)
+        else:
+            cipher = DES.new(self.key, self.mode, iv=self.iv)
+            decrypted = cipher.decrypt(ciphertext[8:])
 
-    def decrypt(self, text):
-        decrypted_text = []  # Список для збереження дешифрованих слів
-        words = text.split('  ')  # Текст розбивається по пробілах на пари
+        return self.unpad(decrypted)
 
-        for word in words:
-            decrypted_word = []  # Тимчасовий список для одного дешифрованого слова
-            pairs = word.split()  # Розбиваємо кожну пару на символи
+    @staticmethod
+    def pad(text):
+        padding_len = 8 - (len(text) % 8)
+        return text + bytes([padding_len]) * padding_len
 
-            for pair in pairs:
-                if len(pair) == 3 and pair[0].isdigit() and pair[1] == '/' and pair[2].isdigit():
-                    i, j = int(pair[0]), int(pair[2])
-                    decrypted_word.append(self.table[i][j])  # Додаємо відновлений символ до поточного слова
-                else:
-                    decrypted_word.append(pair)  # Якщо символ не знайдено, залишаємо його як є
-
-            decrypted_text.append(''.join(decrypted_word))
-
-        return ' '.join(decrypted_text)
-
+    @staticmethod
+    def unpad(text):
+        padding_len = text[-1]
+        return text[:-padding_len]
 
